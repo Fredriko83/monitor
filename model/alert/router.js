cd2c0a181ff940409e5fc64af016d16ffdfff155
fredriko83 <folep02@student.lnu.se>
2017-10-21 18:14:45 +0200

    small changes to alert route

--- model/alert/router.js
+++ model/alert/router.js
@@ -4,16 +4,12 @@ const monitor = require('../../lib/monitor');
 
 router.get('/start', (req, res) => {
   monitor.monitorStart();
-  res.status(200);
-  // .then(collection => res.status(200).json(collection))
-  // .catch(err => next(err));
+  res.json({ alert: 'Started' });
 });
 
 router.get('/stop', (req, res) => {
   monitor.monitorStop();
-  res.status(200);
-  // .then(collection => res.status(200).json(collection))
-  // .catch(err => next(err));
+  res.json({ alert: 'Stopped' });
 });
 
 module.exports = router;
