b026feed85ee5c6778506d7556523ac840469ee8
fredriko83 <folep02@student.lnu.se>
2017-11-04 16:58:00 +0100

    testing schemas

--- model/server/schema.js
+++ model/server/schema.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+//const Timestamp = require('../times')
 
 const serverSchema = new Schema({
   name: { type: String, required: true },
-  address: { type: String, required: true }
+  address: { type: String, required: true },
+  timestamps: [{ type: Schema.Types.ObjectId, ref: 'Timestamp' }]
+  //timestamps: [Timestamp]
 });
-
 
 module.exports = mongoose.model('Server', serverSchema);
--- model/timestamp/schema.js
+++ model/timestamp/schema.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const timestampSchema = new Schema({
-  date: { type: Date, required: true },
+  date: { type: Number, required: true },
   active: { type: Boolean, required: true },
   responseTime: { type: Number, required: true },
-  server: { type: Schema.Types.ObjectId, ref: 'server' }
+  server: { type: Schema.Types.ObjectId, ref: 'Server' }
 });
 
 module.exports = mongoose.model('Timestamp', timestampSchema);
