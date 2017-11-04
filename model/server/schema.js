66ea97dd9f2295ba3006759f0b8c914ffcde946e
fredriko83 <folep02@student.lnu.se>
2017-11-04 13:11:56 +0100

    timestamp

--- model/server/schema.js
+++ model/server/schema.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-
 const serverSchema = new Schema({
   name: { type: String, required: true },
   address: { type: String, required: true }
